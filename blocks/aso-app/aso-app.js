873468d409b57b5ac13164edf0f26840fbfc67bf
Chris Millar <cmillar@adobe.com>
2025-11-17 14:24:28 -0700

    WIP: minor UX polish

--- blocks/aso-app/aso-app.js
+++ blocks/aso-app/aso-app.js
@@ -3,21 +3,31 @@ const VALIDATIONS = {
   description: { length: 250 },
 }
 
-function buildErrorRow(row, key, type, expected, received) {
+function buildSuccessRow(row) {
   const div = document.createElement('div');
-  div.className = 'error';
-  div.textContent = `There was an error validating ${key}. Expected: ${expected} ${type}. Received: ${received} ${type}.`;
+  div.className = 'note success';
+  div.textContent = `The content is valid.`;
 
-  row.insertAdjacentElement('afterend', div);
+  row.append(div);
 }
 
+
+function buildErrorRow(row, key, type, expected, received) {
+  const div = document.createElement('div');
+  div.className = 'note error';
+  div.textContent = `Error validating ${key}. Expected: ${expected} ${type}. Received: ${received} ${type}.`;
+
+  row.append(div);
+}
 
 function validateRow(row, key, dataEl) {
   const rules = VALIDATIONS[key];
   const text = dataEl.innerHTML;
   if (text.length > rules.length) {
     console.log('error');
-    buildErrorRow(row, key, 'length', rules.length, text.length)
+    buildErrorRow(row, key, 'characters', rules.length, text.length);
+  } else {
+    buildSuccessRow(row);
   }
 }
 
@@ -35,6 +45,7 @@ function decorateRow(row) {
 
 export default function init(el) {
   const rows = el.querySelectorAll(':scope > div');
+  if (rows.length === 0) return;
   rows.forEach((row) => {
     row.classList.add('data-row');
     if (row.children) decorateRow(row);
