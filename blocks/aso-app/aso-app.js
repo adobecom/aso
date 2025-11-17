c704c58a7c4d0ba103823e214c473e0e262830d3
Chris Millar <cmillar@adobe.com>
2025-11-17 16:35:10 -0700

    WIP: added google play preview

--- blocks/aso-app/aso-app.js
+++ blocks/aso-app/aso-app.js
@@ -11,6 +11,25 @@ function buildSuccessRow(row) {
   row.append(div);
 }
 
+async function showPreview(meta) {
+  const resp = await fetch('/mocks/play-store.html');
+  if (!resp.ok) {
+    console.log('could not get html');
+    return;
+  }
+  let html = await resp.text();
+  html = html
+    .replaceAll('{TITLE}', meta.title.text)
+    .replaceAll('{DEVELOPER}', meta.developer.text)
+    .replaceAll('{DESCRIPTION}', meta.description.text);
+
+
+  const doc = new DOMParser().parseFromString(html, 'text/html');
+  const styles = doc.head.querySelectorAll('link');
+  document.head.append(...styles);
+  document.body.innerHTML = doc.body.innerHTML;
+}
+
 
 function buildErrorRow(row, key, type, expected, received) {
   const div = document.createElement('div');
@@ -43,11 +62,30 @@ function decorateRow(row) {
   }
 }
 
+const getMetadata = (el) => [...el.childNodes].reduce((rdx, row) => {
+  if (row.children) {
+    const key = row.children[0].textContent.trim().toLowerCase();
+    const content = row.children[1];
+    const text = content.textContent.trim();
+    if (key && text) rdx[key] = { text };
+  }
+  return rdx;
+}, {});
+
 export default function init(el) {
+  const meta = getMetadata(el);
+
   const rows = el.querySelectorAll(':scope > div');
   if (rows.length === 0) return;
   rows.forEach((row) => {
     row.classList.add('data-row');
     if (row.children) decorateRow(row);
   });
+
+  const btn = document.createElement('button');
+  btn.textContent = 'Preview Play Store';
+  btn.addEventListener('click', () => {
+    showPreview(meta);
+  });
+  el.append(btn);
 }
